af237373cdea04c9738dc9919fcb8b4f7ba9fc15
Lucas Andreatta <drea.pierra@gmail.com>
2024-03-15 17:47:29 -0300

    removing useless code

--- Program.cs
+++ Program.cs
@@ -1,42 +1,6 @@
 using BiometricService;
 using Microsoft.Extensions.Logging.Configuration;
 using Microsoft.Extensions.Logging.EventLog;
-using CliWrap;
-
-const string ServiceName = "Fingertech Biometric API";
-
-if (args is { Length: 1 })
-{
-    try
-    {
-        string executablePath =
-            Path.Combine(AppContext.BaseDirectory, "BiometricService.exe");
-
-        if (args[0] is "/Install")
-        {
-            await Cli.Wrap("sc")
-                .WithArguments(new[] { "create", ServiceName, $"binPath={executablePath}", "start=auto" })
-                .ExecuteAsync();
-        }
-
-        else if (args[0] is "/Uninstall")
-        {
-            await Cli.Wrap("sc")
-                .WithArguments(new[] { "stop", ServiceName })
-                .ExecuteAsync();
-
-            await Cli.Wrap("sc")
-                .WithArguments(new[] { "delete", ServiceName })
-                .ExecuteAsync();
-        }
-    }
-    catch (Exception ex)
-    {
-        Console.WriteLine(ex);
-    }
-
-    return;
-}
 
 var builder = WebApplication.CreateBuilder(args);
 
